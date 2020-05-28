76cc6d96b2393fba4dbc55df0704170ddc53fdae
Ashay Parikh <ashayparikh22@gmail.com>
2020-05-28 13:00:23 -0500

    Update Rand.cs

--- C-Sharp-Neat/Rand.cs
+++ C-Sharp-Neat/Rand.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class Rand
 {
     public static int randomInt(int min, int max)
     {
-        return (int)(Random.Range(0f,1f) * (max - min + 1)) + min;
+        Random random = new Random();    
+        return (int)(random.Next(0, 1) * (max - min + 1)) + min;
     }
 
     public static double randomNumber(double min, double max)
     {
-        return Random.Range(0f, 1f) * (max - min) + min;
+        Random random = new Random();  
+        return random.Next(0, 1) * (max - min) + min;
     }
 }
